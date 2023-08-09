eabcc202dc1c0bc9c187116c7e3e826118c867c9
Bilal Ahmad Parray <122676498+bilalparray07@users.noreply.github.com>
2023-08-09 16:14:55 +0530

    Add files via upload

--- ranking.js
+++ ranking.js
@@ -1,150 +1,142 @@
 //Bilal Starts here
 let bilal = {
-  image:'images/bilal.png',
-  name:"Bilal Parray",
-  role:"Batsman",
-  profile:"players/bilal.html",
-
-}
-document.getElementById('player1image').src=         bilal.image;
-document.getElementById('player1name').innerHTML =   bilal.name;
-document.getElementById('player1role').innerHTML =   bilal.role;
-document.getElementById('player1button').href = bilal.profile;
+  image: "images/bilal.png",
+  name: "Bilal Parray",
+  role: "Batsman",
+  profile: "players/bilal.html",
+};
+document.getElementById("player1image").src = bilal.image;
+document.getElementById("player1name").innerHTML = bilal.name;
+document.getElementById("player1role").innerHTML = bilal.role;
+document.getElementById("player1button").href = bilal.profile;
 
 //owais starts her1
 let owais = {
-  image:'images/owais.png',
-  name:"Owais Ahmad",
-  role:"All Rounder",
-  profile:"players/owais.html"
-}
-document.getElementById('player3image').src=         owais.image;
-document.getElementById('player3name').innerHTML =   owais.name;
-document.getElementById('player3role').innerHTML =   owais.role;
-document.getElementById('player3button').href =      owais.profile;
-
+  image: "images/owais.png",
+  name: "Owais Ahmad",
+  role: "All Rounder",
+  profile: "players/owais.html",
+};
+document.getElementById("player3image").src = owais.image;
+document.getElementById("player3name").innerHTML = owais.name;
+document.getElementById("player3role").innerHTML = owais.role;
+document.getElementById("player3button").href = owais.profile;
 
 //ubaid starts here
 
 let ubi = {
-  image:'images/ubi.png',
-  name:"Ubi Obaid",
-  role:"Wk Batsman",
-  profile:"players/ubi.html"
-}
-document.getElementById('player2image').src=         ubi.image;
-document.getElementById('player2name').innerHTML =   ubi.name;
-document.getElementById('player2role').innerHTML =   ubi.role;
-document.getElementById('player2button').href =      ubi.profile;
+  image: "images/ubi.png",
+  name: "Ubi Obaid",
+  role: "Wk Batsman",
+  profile: "players/ubi.html",
+};
+document.getElementById("player2image").src = ubi.image;
+document.getElementById("player2name").innerHTML = ubi.name;
+document.getElementById("player2role").innerHTML = ubi.role;
+document.getElementById("player2button").href = ubi.profile;
 
 ///ahsaan starts here
 let ehsaan = {
-  image:'images/ehsaan.png',
-  name:"Ahsaan Ul Haq",
-  role:"All Rounder",
-  profile:"players/ehsaan.html"
-}
-document.getElementById('player4image').src=         ehsaan.image;
-document.getElementById('player4name').innerHTML =   ehsaan.name;
-document.getElementById('player4role').innerHTML =   ehsaan.role;
-document.getElementById('player4button').href =      ehsaan.profile;
+  image: "images/ehsaan.png",
+  name: "Ahsaan Ul Haq",
+  role: "All Rounder",
+  profile: "players/ehsaan.html",
+};
+document.getElementById("player4image").src = ehsaan.image;
+document.getElementById("player4name").innerHTML = ehsaan.name;
+document.getElementById("player4role").innerHTML = ehsaan.role;
+document.getElementById("player4button").href = ehsaan.profile;
 
 //Suhail starts here
 let suhail = {
-  image:'images/suhail.png',
-  name:"Suhail Parray",
-  role:"All Rounder",
-  profile:"players/suhail.html"
-}
-document.getElementById('player5image').src=         suhail.image;
-document.getElementById('player5name').innerHTML =   suhail.name;
-document.getElementById('player5role').innerHTML =   suhail.role;
-document.getElementById('player5button').href =      suhail.profile;
+  image: "images/suhail.png",
+  name: "Suhail Parray",
+  role: "All Rounder",
+  profile: "players/suhail.html",
+};
+document.getElementById("player5image").src = suhail.image;
+document.getElementById("player5name").innerHTML = suhail.name;
+document.getElementById("player5role").innerHTML = suhail.role;
+document.getElementById("player5button").href = suhail.profile;
 
 //maxi starts here
 let muzamil = {
-  image:'images/muzamil.png',
-  name:"Muzamil Fayaz",
-  role:"All Rounder",
-  profile:"players/muzamil.html"
-}
-document.getElementById('player6image').src=         muzamil.image;
-document.getElementById('player6name').innerHTML =   muzamil.name;
-document.getElementById('player6role').innerHTML =   muzamil.role;
-document.getElementById('player6button').href =      muzamil.profile;
-
+  image: "images/muzamil.png",
+  name: "Muzamil Fayaz",
+  role: "All Rounder",
+  profile: "players/muzamil.html",
+};
+document.getElementById("player6image").src = muzamil.image;
+document.getElementById("player6name").innerHTML = muzamil.name;
+document.getElementById("player6role").innerHTML = muzamil.role;
+document.getElementById("player6button").href = muzamil.profile;
 
 ///Ishtiyaq
 let ishtiyaq = {
-  image:'images/ishtiyaq.png',
-  name:"Ishtiyaq Ayoub",
-  role:"All Rounder",
-  profile:"players/ishtiyaq.html"
-}
-document.getElementById('player7image').src=         ishtiyaq.image;
-document.getElementById('player7name').innerHTML =   ishtiyaq.name;
-document.getElementById('player7role').innerHTML =   ishtiyaq.role;
-document.getElementById('player7button').href =      ishtiyaq.profile;
-
+  image: "images/ishtiyaq.png",
+  name: "Ishtiyaq Ayoub",
+  role: "All Rounder",
+  profile: "players/ishtiyaq.html",
+};
+document.getElementById("player7image").src = ishtiyaq.image;
+document.getElementById("player7name").innerHTML = ishtiyaq.name;
+document.getElementById("player7role").innerHTML = ishtiyaq.role;
+document.getElementById("player7button").href = ishtiyaq.profile;
 
 ///sahil
 let sahil = {
-  image:'images/sahil.png',
-  name:"Sahil Nazir",
-  role:"Batsman",
-  profile:"players/sahil.html"
-}
-document.getElementById('player8image').src=         sahil.image;
-document.getElementById('player8name').innerHTML =   sahil.name;
-document.getElementById('player8role').innerHTML =   sahil.role;
-document.getElementById('player8button').href =      sahil.profile;
+  image: "images/sahil.png",
+  name: "Sahil Nazir",
+  role: "Batsman",
+  profile: "players/sahil.html",
+};
+document.getElementById("player8image").src = sahil.image;
+document.getElementById("player8name").innerHTML = sahil.name;
+document.getElementById("player8role").innerHTML = sahil.role;
+document.getElementById("player8button").href = sahil.profile;
 
 ///showket
 let showket = {
-  image:'images/showket.png',
-  name:"Showket Parray",
-  role:"Batsman",
-  profile:"players/showket.html"
-}
-document.getElementById('player9image').src=         showket.image;
-document.getElementById('player9name').innerHTML =   showket.name;
-document.getElementById('player9role').innerHTML =   showket.role;
-document.getElementById('player9button').href =      showket.profile;
+  image: "images/showket.png",
+  name: "Showket Parray",
+  role: "Batsman",
+  profile: "players/showket.html",
+};
+document.getElementById("player9image").src = showket.image;
+document.getElementById("player9name").innerHTML = showket.name;
+document.getElementById("player9role").innerHTML = showket.role;
+document.getElementById("player9button").href = showket.profile;
 
 //zahid startshere
 let zahid = {
-  image:'images/zahid.png',
-  name:"Zahid Bhat",
-  role:"Batsman",
-  profile:"players/zahid.html"
-}
-document.getElementById('player10image').src=         zahid.image;
-document.getElementById('player10name').innerHTML =   zahid.name;
-document.getElementById('player10role').innerHTML =   zahid.role;
-document.getElementById('player10button').href =      zahid.profile;
+  image: "images/zahid.png",
+  name: "Zahid Bhat",
+  role: "Batsman",
+  profile: "players/zahid.html",
+};
+document.getElementById("player10image").src = zahid.image;
+document.getElementById("player10name").innerHTML = zahid.name;
+document.getElementById("player10role").innerHTML = zahid.role;
+document.getElementById("player10button").href = zahid.profile;
 
 ///liyaqat
 let liyaqat = {
-  image:'images/liyaqat.png',
-  name:"Liyaqat Bhat",
-  role:"Bowler",
-  profile:"players/liyaqat.html"
-}
-document.getElementById('player11image').src=         liyaqat.image;
-document.getElementById('player11name').innerHTML =   liyaqat.name;
-document.getElementById('player11role').innerHTML =   liyaqat.role;
-document.getElementById('player11button').href =      liyaqat.profile;
+  image: "images/liyaqat.png",
+  name: "Liyaqat Bhat",
+  role: "Bowler",
+  profile: "players/liyaqat.html",
+};
+document.getElementById("player11image").src = liyaqat.image;
+document.getElementById("player11name").innerHTML = liyaqat.name;
+document.getElementById("player11role").innerHTML = liyaqat.role;
+document.getElementById("player11button").href = liyaqat.profile;
 
-
-
-// mom 
-// document.getElementById('momsection').classList.add('d-none');
-document.getElementById('mom').innerText = "Liyaqat Bhat";
-document.getElementById('momimage').src ="images/liyaqat.png";
-document.getElementById('mompara').innerText = `
+// mom
+document.getElementById("momsection").classList.add("d-none");
+document.getElementById("mom").innerText = "Liyaqat Bhat";
+document.getElementById("momimage").src = "images/liyaqat.png";
+document.getElementById("mompara").innerText = `
 Congratulations to Liyaqat Bhat for his outstanding performance in the cricket game! His remarkable talent and skill on the field led to the impressive feat of taking five wickets. Liyaqat's exceptional bowling display not only showcased his ability to deceive and outwit the batsmen but also played a crucial role in turning the tide in favor of his team.
 
 Taking five wickets in a cricket match is a testament to Liyaqat's hard work, dedication, and perseverance in honing his craft. His ability to consistently produce match-winning performances is truly commendable and sets him apart as a valuable asset to the team.
-`
-
-
+`;
