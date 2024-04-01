9f6b1cd1c80c584078c6e5d756746bb0b18eee95
Bilal Ahmad <dev.renosoftwares@gmail.com>
2024-04-01 15:40:31 +0530

    added rozdari 3rd sunday

--- array.js
+++ array.js
@@ -1,62 +1,62 @@
 //Batting Array 2024
-const BilalScoreArray = [53, 5, 0, 14, 0, 10, 0, 7, 2, 14];
-const BilalLastFourArray = [10, 14];
+const BilalScoreArray = [53, 5, 0, 14, 0, 10, 0, 7, 2, 14, 28];
+const BilalLastFourArray = [10, 14, 28];
 // ///////////////////////////////////
-const OwaisScoreArray = [14, 42, 14, 0, 19, 10, 19, 0, 6, 1];
-const OwaisLastFourArray = [10, 1];
+const OwaisScoreArray = [14, 42, 14, 0, 19, 10, 19, 0, 6, 1, 49];
+const OwaisLastFourArray = [10, 1, 49];
 // /////////////////////////////////////
-const SahilScoreArray = [4, 10, 60, 26, 42, 6, 42, 0, 18, 24];
-const SahilLastFourArray = [6, 24];
+const SahilScoreArray = [4, 10, 60, 26, 42, 6, 42, 0, 18, 25, 5];
+const SahilLastFourArray = [6, 25, 5];
 // /////////////////////////////////////
 
-const EhsaanScoreArray = [0, 0, 0, 0, 0, 10, 0, 0, 14];
-const EhsaanLastFourArray = [10, 14];
+const EhsaanScoreArray = [0, 0, 0, 0, 0, 10, 0, 0, 14, 0];
+const EhsaanLastFourArray = [10, 14, 0];
 // ////////////////////////////////////
-const ZahidScoreArray = [43, 59, 26, 19, 22, 11, 22, 49, 6, 11];
-const ZahidLastFourArray = [11, 11];
+const ZahidScoreArray = [43, 59, 26, 19, 22, 11, 22, 49, 6, 11, 4];
+const ZahidLastFourArray = [11, 11, 4];
 // /////////////////////////////////////////
-const SuhailScoreArray = [25, 13, 12, 35, 0, 43, 0, 0, 28, 52];
-const SuhailLastFourArray = [43, 52];
+const SuhailScoreArray = [25, 13, 12, 35, 0, 43, 0, 0, 28, 52, 2];
+const SuhailLastFourArray = [43, 52, 2];
 ///////////////////////////////////////////
-const IshtiyaqScoreArray = [2, 10, 0, 34, 52, 46, 52, 0, 4, 11];
-const IshtiyaqLastFourArray = [46, 11];
+const IshtiyaqScoreArray = [2, 10, 0, 34, 52, 46, 52, 0, 4, 11, 0];
+const IshtiyaqLastFourArray = [46, 11, 0];
 // //////////////////////////////////////////
-const MuzamilScoreArray = [0, 40, 28, 56, 18, 15, 13, 0, 36, 20];
-const MuzamilLastFourArray = [15, 20];
+const MuzamilScoreArray = [0, 40, 28, 56, 18, 15, 13, 0, 36, 20, 32];
+const MuzamilLastFourArray = [15, 20, 32];
 // /////////////////////////////////////////
-const UbiScoreArray = [17, 10, 35, 1, 0, 10, 0, 34, 38, 0];
-const UbiLastFourArray = [10, 0];
+const UbiScoreArray = [17, 10, 35, 1, 0, 10, 0, 34, 38, 0, 1];
+const UbiLastFourArray = [10, 0, 1];
 // /////////////////////////////////////////
-const LiyaqatScoreArray = [0, 5, 13, 3, 13, 14, 13, 0, 58, 21];
-const LiyaqatLastFourArray = [14, 21];
+const LiyaqatScoreArray = [0, 5, 13, 3, 13, 14, 13, 0, 58, 21, 10];
+const LiyaqatLastFourArray = [14, 21, 10];
 // ////////////////////////////////////////
-const ShowketScoreArray = [17, 6, 0, 14, 21, 14, 21, 0, 0, 8];
-const ShowketLastFourArray = [14, 8];
+const ShowketScoreArray = [17, 6, 0, 14, 21, 14, 21, 0, 0, 8, 2];
+const ShowketLastFourArray = [14, 8, 2];
 // ///////////////////////////////////////////
 
 //Balls Faced  Array 2204
 
-const BilalBallsArray = [46, 8, 16, 4];
-const OwaisBallsArray = [16, 80, 2];
-const SahilBallsArray = [4, 14, 47, 68, 19];
+const BilalBallsArray = [46, 8, 16, 4, 12];
+const OwaisBallsArray = [16, 80, 2, 30];
+const SahilBallsArray = [4, 14, 47, 68, 19, 7];
 const EhsaanBallsArray = [0, 1, 10];
-const ZahidBallsArray = [26, 70, 36, 20];
-const SuhailBallsArray = [68, 21, 105, 27];
+const ZahidBallsArray = [26, 70, 36, 20, 2];
+const SuhailBallsArray = [68, 21, 105, 27, 5];
 const IshtiyaqBallsArray = [4, 3, 83, 16];
-const MuzamilBallsArray = [1, 65, 58, 17];
-const UbiBallsArray = [12, 28, 94, 2];
-const LiyaqatBallsArray = [1, 28, 55, 22];
-const ShowketBallsArray = [9, 12, 20, 11];
+const MuzamilBallsArray = [1, 65, 58, 17, 33];
+const UbiBallsArray = [12, 28, 94, 2, 10];
+const LiyaqatBallsArray = [1, 28, 55, 22, 10];
+const ShowketBallsArray = [9, 12, 20, 11, 5];
 
 //Wickets  Array 2024
 const BilalWicketArray = [0];
 const OwaisWicketArray = [2, 1, 5, 3, 3, 3, 1, 2];
 const SahilWicketArray = [2];
 const EhsaanWicketArray = [0];
-const ZahidWicketArray = [0, 1, 2, 2, 1, 1, 3, 1];
+const ZahidWicketArray = [0, 1, 2, 2, 1, 1, 3, 1, 1];
 const SuhailWicketArray = [0, 3, 2, 2];
 const IshtiyaqWicketArray = [1, 1, 3, 4, 1, 3, 1, 2];
-const MuzamilWicketArray = [4, 1, 1, 4, 1, 2, 1];
+const MuzamilWicketArray = [4, 1, 1, 4, 1, 2, 1, 3];
 const UbiWicketArray = [0];
 const LiyaqatWicketArray = [2, 3, 2, 4, 3, 2];
-const ShowketWicketArray = [0];
+const ShowketWicketArray = [1];
